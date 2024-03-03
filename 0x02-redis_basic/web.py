6d7cdae0127f2d60c655432d8f78e9277edb838a
robel <robelwon75@gmail.com>
2024-03-03 15:27:42 +0300

    task 5

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+tarker for the webcache
+"""
+import redis
+import requests
+from functools import wraps
+
+store = redis.Redis()
+
+
+def count_url_access(method):
+    """ counts number of times the url is accessed """
+    @wraps(method)
+    def wrapper(url):
+        c_key = "cached:" + url
+        cached_data = store.get(c_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(c_key, html)
+
+        store.expire(c_key, 10)
+        return html
+    return wrapper
+
+def project_info():
+    ''' returns the projcet info '''
+    return "Web file"
+
+
+@count_url_access
+def get_page(url: str) -> str:
+    """ returns the html """
+    res = requests.get(url)
+    return res.text
