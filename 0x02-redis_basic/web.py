ece1dcc22387e3cb712c6742cae2b9b04611934a
robel <robelwon75@gmail.com>
2024-03-03 16:08:51 +0300

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -21,7 +21,7 @@ def count_requests(method: Callable) -> Callable:
             return cached_html.decode('utf-8')
         html = method(url)
 
-        redis_.setex(f"cached:{url}", 10, html)
+        redis_.psetex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
