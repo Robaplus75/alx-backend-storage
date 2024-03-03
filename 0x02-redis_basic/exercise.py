439654e2a4a0bad142e0336b1fd4dbd6d4225487
robel <robelwon75@gmail.com>
2024-03-03 14:51:58 +0300

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,3 +19,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get_str(self, key: str) -> str:
+        '''for parametrizing Cache.get'''
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''for converting the data to the desired format'''
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_int(self, key: str) -> int:
+        '''for parametrizing Cache.get with correct conversion function'''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
