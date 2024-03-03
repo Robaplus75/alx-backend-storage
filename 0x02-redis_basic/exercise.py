eaa0b3d962ff069afcf9516efd1e40bd8bebcb81
robel <robelwon75@gmail.com>
2024-03-03 14:22:32 +0300

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Cache Module """
+
+import redis
+import uuid
+
+class Cache:
+    """ redis cache class """
+
+    def __init__(self):
+        """ INITIALIZATION """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    
+    def store(self, data: str | bytes | int | float) -> str:
+        """ store data to redis """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
