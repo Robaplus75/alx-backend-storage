02b04348bd6ada31ea662a2b78809aee7ae9cc7f
robel <robelwon75@gmail.com>
2024-03-03 14:31:03 +0300

    task 0: type-annotation fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store data to redis """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
